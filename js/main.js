ca1c418b930d12cad3a71b9266229950a1e6d7eb
akino.ikumi <akinoproject@outlook.com>
2018-04-11 21:10:08 +0900

    色々追加

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+   // #で始まるアンカーをクリックした場合に処理
+   $('a[href^="#"]').click(function() {
+      var speed    = 300;
+      var href     = $(this).attr("href");
+      var target   = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top - 56;
+
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+
+      return false;
+   });
+
+});
