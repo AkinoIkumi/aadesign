776a65a6211fe3ddfecbaf96e093f38196cf4e17
akino.ikumi <akinoproject@outlook.com>
2018-04-15 12:53:17 +0900

    作業中

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 $(function(){
 
-   // #で始まるアンカーをクリックした場合に処理
-   $('a[href^="#"]').click(function() {
-      var speed    = 300;
-      var href     = $(this).attr("href");
-      var target   = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top - 56;
+    // #で始まるアンカーをクリックした場合に処理
+    $('a[href^="#"]').click(function() {
+        var speed    = 300;
+        var href     = $(this).attr("href");
+        var target   = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - 56;
 
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
 
-      return false;
-   });
+        return false;
+    });
+
+    $("#header").load("/template/header.html");
+    $("#footer").load("/template/footer.html");
 
 });
